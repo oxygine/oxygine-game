184ad4b4f988b8b876eb3d87c6092a8f43ce0a59
dmuratshin <dmuratshin@gmail.com>
2016-04-17 14:55:02 +0500

    unified cmake emscripten

--- my_awesome_game/project/proj.emscripten/build.bat
+++ my_awesome_game/project/proj.emscripten/build.bat
@@ -1,7 +1,7 @@
-python ../../../oxygine-framework//tools/others/embed_folder_js.py -s ../data
+python ../../../oxygine-framework/tools/others/embed_folder_js.py -s ../../data
 
 mkdir build
 cd build
-cmake -DCMAKE_TOOLCHAIN_FILE="%EMSCRIPTEN%/cmake/Modules/Platform/emscripten.cmake" -G"Unix Makefiles" .. 
+cmake -DCMAKE_TOOLCHAIN_FILE="%EMSCRIPTEN%/cmake/Modules/Platform/emscripten.cmake" -G"Unix Makefiles" ../../proj.cmake 
 make
 cd ..
--- my_awesome_game/project/proj.emscripten/build_release.bat
+++ my_awesome_game/project/proj.emscripten/build_release.bat
@@ -1,7 +1,7 @@
-python ../../../oxygine-framework//tools/others/embed_folder_js.py -s ../data
+python ../../../oxygine-framework//tools/others/embed_folder_js.py -s ../../data
 
 mkdir build_release
 cd build_release
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE="%EMSCRIPTEN%/cmake/Modules/Platform/emscripten.cmake" -G"Unix Makefiles" .. 
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE="%EMSCRIPTEN%/cmake/Modules/Platform/emscripten.cmake" -G"Unix Makefiles" ../../proj.cmake 
 make
 cd ..
