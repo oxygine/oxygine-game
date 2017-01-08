17dbd13e03b5d7d5742fc3d5e9cbc6b5e77cc2cc
dmuratshin <frankinshtein85@gmail.com>
2017-01-09 03:45:54 +0500

    min

---
+++ my_awesome_game/project/proj.cmake/build_emsc.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+emsdk activate
+
+python ../../../oxygine-framework/tools/others/embed_folder_js.py -s ../../data
+
+mkdir build_emsc
+cd build_emsc
+cmake -DCMAKE_TOOLCHAIN_FILE="${EMSCRIPTEN}/cmake/Modules/Platform/emscripten.cmake" -G"Unix Makefiles" ..
+make
+cd ..
---
+++ my_awesome_game/project/proj.cmake/build_emsc_release.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+emsdk activate
+
+python ../../../oxygine-framework/tools/others/embed_folder_js.py -s ../../data
+
+mkdir build_emsc_release
+cd build_emsc_release
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE="${EMSCRIPTEN}/cmake/Modules/Platform/emscripten.cmake" -G"Unix Makefiles" ..
+make
+cd ..
